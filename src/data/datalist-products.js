97508b823260a83d60c909ac8e30e0b744dc85cb
formateur-glodiee-tshimini <contact@tshimini.fr>
2025-10-14 18:43:17 +0200

    feat: add product

---
+++ src/data/datalist-products.js
@@ -0,0 +1,115 @@
+export const datalistProductItems = [{
+  label: 'Bananes',
+  value: 'banana',
+  category : 'fruits'
+},
+{
+  label: 'Pommes',
+  value:'apple',
+    category : 'fruits'
+},
+{
+  label: 'oranges',
+  value:'orange',
+    category : 'fruits'
+},
+{
+  label: 'Lait',
+  value:'milk',
+    category : 'prodtuis laitiers'
+},
+{
+  label: 'yaourt',
+  value:'yogurt',
+  category : 'prodtuis laitiers'
+},
+{
+  label: 'fromages',
+  value:'cheese',
+  category : 'prodtuis laitiers'
+},
+{
+  label: 'poulet',
+  value:'chicken',
+  category : 'viandes, poissons et protéines'
+},
+{
+  label: 'oeufs',
+  value:'egg',
+  category : 'viandes, poissons et protéines'
+},
+{
+  label: 'jambon ou dinde',
+  value:'pork',
+  category : 'viandes, poissons et protéines'
+},
+{
+  label: 'lentilles',
+  value:'lentils',
+  category : 'viandes, poissons et protéines'
+},
+{
+  label: 'pâtes - épicerie sèche',
+  value:'pasta',
+  category : 'grocery'
+},
+{
+  label: 'riz - épicerie sèche',
+  value:'rice',
+  category : 'grocery'
+},
+{
+  label: 'céréales',
+  value:'cornflex',
+  category : 'grocery'
+},
+{
+  label: 'farine',
+  value:'flour',
+  category : 'grocery'
+},
+{
+  label: 'sucre',
+  value:'sugar',
+  category : 'grocery'
+},
+{
+  label: 'huile',
+  value:'oil',
+  category : 'grocery'
+},
+{
+  label: 'sauce tomate',
+  value:'tomato sauce',
+  category : 'grocery'
+},
+{
+  label: 'surgelé',
+  value:'frozen',
+  category : 'frozen'
+},
+{
+  label: 'compotes',
+  value:'fruit compotes',
+  category : 'various'
+},
+{
+  label: 'biscuits',
+  value:'biscuit',
+  category : 'various'
+},
+{
+  label: 'chocolat',
+  value:'chocolate',
+  category : 'various'
+},
+{
+  label: 'lessive',
+  value:'detergent',
+  category : 'hygiène'
+},
+{
+  label: 'liquide vaisselle',
+  value:'sugar',
+  category : 'grocery'
+}]
